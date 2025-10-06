1cacb8174b2f19ffb8d8380396408895b79538cf
Mohammadreza Rahmani <rahhmani.mohammadreza@gmail.com>
2025-10-06 08:01:51 +0100

    updating package name

--- feature/forecast/src/androidTest/java/com/forecast/ExampleInstrumentedTest.kt
+++ feature/forecast/src/androidTest/java/com/forecast/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.detail.test", appContext.packageName)
+        assertEquals("com.forecast.test", appContext.packageName)
     }
 }
