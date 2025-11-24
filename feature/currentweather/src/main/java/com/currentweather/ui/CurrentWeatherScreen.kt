3c07d550a98674f5fee579f31a08cf832cdde45f
blueskyredsky <rahhmani.mohammadreza@gmail.com>
2025-11-24 07:24:41 +0000

    working on getting notification permission

--- feature/currentweather/src/main/java/com/currentweather/ui/CurrentWeatherScreen.kt
+++ feature/currentweather/src/main/java/com/currentweather/ui/CurrentWeatherScreen.kt
@@ -5,13 +5,37 @@ import android.content.Intent
 import android.net.Uri
 import android.provider.Settings
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
@@ -20,18 +44,33 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil.compose.AsyncImage
 import com.common.model.ErrorType
+import com.common.model.extension.toFormattedTime
 import com.currentweather.R
+import com.currentweather.data.model.currentweather.CurrentWeather
+import com.currentweather.data.model.forecast.Forecast
+import com.currentweather.ui.component.ConnectedWavyLines
 import com.currentweather.ui.component.CustomSearchBar
 import com.currentweather.ui.component.ErrorItem
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.google.accompanist.permissions.shouldShowRationale
+import java.util.Calendar
+import kotlin.math.roundToInt
 
 @OptIn(ExperimentalPermissionsApi::class, ExperimentalMaterial3Api::class)
 @Composable
@@ -239,5 +278,267 @@ fun CurrentWeatherScreen(
                     )
             }
         }
+    }
+}
+
+@Composable
+fun LoadingContent(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = colorResource(com.common.R.color.pale_blue))
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 24.dp)
+                .systemBarsPadding(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Spacer(modifier = Modifier.height(56.dp))
+            Box(
+                modifier = Modifier
+                    .width(70.dp)
+                    .height(20.dp)
+                    .shimmerEffect()
+            )
+
+            Spacer(modifier = Modifier.height(40.dp))
+            Box(
+                modifier = Modifier
+                    .size(150.dp, 200.dp)
+                    .shimmerEffect()
+            )
+
+            Spacer(modifier = Modifier.height(60.dp))
+            Box(
+                modifier = Modifier
+                    .width(100.dp)
+                    .height(30.dp)
+                    .shimmerEffect()
+            )
+
+            Spacer(modifier = Modifier.height(40.dp))
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(2.dp)
+                    .shimmerEffect()
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+                repeat(6) {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Box(
+                            modifier = Modifier
+                                .size(40.dp)
+                                .clip(CircleShape)
+                                .shimmerEffect()
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Box(
+                            modifier = Modifier
+                                .width(30.dp)
+                                .height(12.dp)
+                                .shimmerEffect()
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+fun Modifier.shimmerEffect(): Modifier = composed {
+    val shimmerColors = listOf(
+        Color.LightGray.copy(alpha = 0.6f),
+        Color.LightGray.copy(alpha = 0.2f),
+        Color.LightGray.copy(alpha = 0.6f),
+    )
+
+    val transition = rememberInfiniteTransition(label = "shimmerTransition")
+    val translateAnimation = transition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1000f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 1000,
+                easing = FastOutSlowInEasing
+            ),
+            repeatMode = RepeatMode.Reverse
+        ), label = "shimmerAnimation"
+    )
+
+    background(
+        brush = Brush.linearGradient(
+            colors = shimmerColors,
+            start = Offset(x = translateAnimation.value, y = 0f),
+            end = Offset(x = translateAnimation.value + 500f, y = 500f)
+        )
+    )
+}
+
+@Composable
+fun SuccessContent(
+    currentWeather: CurrentWeather?,
+    forecast: Forecast?,
+    weatherData: WeatherUI,
+    modifier: Modifier = Modifier,
+    onNavigateToDetail: (cityName: String) -> Unit
+) {
+    val color = colorResource(weatherData.textColorResource)
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(colorResource(weatherData.backgroundColorResource))
+    ) {
+        Image(
+            modifier = Modifier
+                .fillMaxSize()
+                .align(Alignment.Center),
+            painter = painterResource(weatherData.backgroundImageResource),
+            contentDescription = null, // decorative image
+        )
+
+        currentWeather?.let { weather ->
+            Column(
+                modifier = Modifier
+                    .verticalScroll(rememberScrollState())
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                IconButton(
+                    modifier = Modifier.align(Alignment.End),
+                    onClick = { /*TODO*/ }) {
+                    Icon(
+                        painter = painterResource(com.common.R.drawable.ic_notifications),
+                        contentDescription = null,
+                        tint = colorResource(weatherData.textColorResource)
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(56.dp))
+
+                Text(
+                    text = stringResource(R.string.today),
+                    style = MaterialTheme.typography.titleLarge,
+                    color = color
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text(
+                    text = "${weather.current.tempC.roundToInt()}°",
+                    style = MaterialTheme.typography.displayLarge.copy(fontSize = 100.sp),
+                    color = color
+                )
+
+                Spacer(modifier = Modifier.height(100.dp))
+
+                weather.location.name.let { cityName ->
+                    Icon(
+                        painter = painterResource(com.common.R.drawable.ic_loaction),
+                        contentDescription = null,
+                        tint = color
+                    )
+
+                    Text(
+                        text = cityName,
+                        style = MaterialTheme.typography.displayLarge,
+                        color = color
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(40.dp))
+
+                ConnectedWavyLines(
+                    modifier = Modifier.padding(horizontal = 8.dp),
+                    color = color
+                )
+
+                forecast?.forecast?.forecastDay?.first()?.hour?.let { hours ->
+                    val listState = rememberLazyListState()
+                    LazyRow(state = listState) {
+                        items(hours, key = { it.time.hashCode() }) {
+                            ItemHourlyForecast(
+                                iconUrl = it.condition.icon,
+                                temperature = it.tempC.toString(),
+                                time = it.time,
+                                color = color
+                            )
+                        }
+                    }
+
+                    LaunchedEffect(key1 = hours) {
+                        val currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+                        val targetIndex = hours.indexOfFirst {
+                            it.time.substringAfter(" ").substringBefore(":").toInt() == currentHour
+                        }
+                        if (targetIndex != -1) {
+                            listState.scrollToItem(targetIndex) // scroll to current time
+                        }
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // todo navigate to detail
+                /*weather.location.name.let { cityName ->
+                    Button(
+                        onClick = { onNavigateToDetail(cityName) },
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(
+                            text = stringResource(R.string.view_detail, cityName)
+                        )
+                    }
+                }*/
+            }
+        } ?: run {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    textAlign = TextAlign.Center,
+                    text = stringResource(R.string.no_weather_data_available)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun ItemHourlyForecast(
+    iconUrl: String,
+    temperature: String,
+    time: String,
+    color: Color,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = time.toFormattedTime(),
+            style = MaterialTheme.typography.titleSmall,
+            color = color
+        )
+        AsyncImage(
+            model = "https:$iconUrl",
+            contentDescription = stringResource(R.string.weather_condition_icon),
+            modifier = Modifier.size(32.dp)
+        )
+        Text(
+            text = "$temperature°",
+            style = MaterialTheme.typography.titleSmall,
+            color = color
+        )
     }
 }
